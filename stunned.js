4b6ef4bc445c9830b3a5fe14589789c78499743d
Nicolas Lukan <nicolaslukan@students.berkeley.net>
2017-05-31 10:51:01 -0700

    we allmost done Biiiiiiiiiitchhh

--- stunned.js
+++ stunned.js
@@ -1,3 +1,18 @@
 /**
  * Created by h205p3 on 5/18/17.
  */
+
+$(document).ready(function() {
+
+    var data = getData();
+
+    var totalTeams = function() {
+
+        //count living users in data
+        var livingUsers = users.....
+
+        $("#theID").text(livingUsers);
+
+    };
+
+});
